2dbe8f150b244e28fb2161490118ae7db2a01e85
Heitz Robin <heitz@igmr.rwth-aachen.de>
2024-02-02 21:56:31 +0100

    little changes

--- install/ubuntu/zsh_ubuntu.sh
+++ install/ubuntu/zsh_ubuntu.sh
@@ -7,12 +7,11 @@ sudo apt update && sudo apt install zsh
 # Install Oh-my-zsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
 # Oh my zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 echo '# -------------- ohmyzsh install---------------' >> $ZSHRC
 echo "source $DOTFILES/shell/ohmyzsh.sh" >> $ZSHRC
