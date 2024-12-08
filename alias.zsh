a1623f28e66bb0a80ead6c36a3ff4353af6aecae
Robin Heitz <robin.heitz@rwth-aachen.de>
2024-12-08 20:15:51 +0100

    Check for humble install

--- alias.zsh
+++ alias.zsh
@@ -21,8 +21,9 @@ alias di='docker images'
 alias dc='docker container ls -a'
 
 # ros2
-source /opt/ros/humble/setup.zsh
-export ROS_WS=~/dev/ros2/ros2_ws
-alias b='cd $ROS_WS && colcon build --symlink-install && source install/setup.zsh'
-alias s='cd $ROS_WS && source install/setup.zsh'
-
+if [ -f /opt/ros/humble/setup.zsh ]; then
+  source /opt/ros/humble/setup.zsh
+  export ROS_WS=~/dev/ros2/ros2_ws
+  alias b='cd $ROS_WS && colcon build --symlink-install && source install/setup.zsh'
+  alias s='cd $ROS_WS && source install/setup.zsh'
+fi
